dd53d7dc20812cabfc401fccda2f3cfd5af9c667
drogon98 <h21948298@gmail.com>
2022-10-08 12:36:14 +0300

    chore:addedbase layer class

---
+++ src/layers/base.py
@@ -0,0 +1,39 @@
+import pickle
+import numpy as np
+
+from os import path,makedirs,remove
+
+
+
+class Layer(object):
+
+    def init_cache(self):
+        cache = dict()
+        cache['dW'] = np.zeros_like(self.params.get('W'))
+        cache['db'] = np.zeros_like(self.params.get('b'))
+        return cache
+
+    def save_weights(self,dump_path):
+        dump_cache = {
+            'cache': self.cache,
+            'grads': self.grads,
+            'momentum': self.momentum_cache,
+            'rmsprop': self.rmsprop_cache
+        }
+        save_path = path.join(dump_path,f"{self.name}.pickle")
+        makedirs(path.dirname(save_path),exist_ok=True)
+        remove(save_path)
+        with open(save_path,"wb") as d:
+            pickle.dump(dump_cache,d)
+
+    
+    def load_weights(self,dump_path):
+        if self.name == None:
+            self.name="conv"
+        read_path = path.join(dump_path, self.name+'.pickle')
+        with open(read_path, 'rb') as r:
+            dump_cache = pickle.load(r)
+        self.cache = dump_cache.get('cache')
+        self.grads = dump_cache.get('grads')
+        self.momentum_cache = dump_cache.get('momentum')
+        self.rmsprop_cache = dump_cache.get('rmsprop')
